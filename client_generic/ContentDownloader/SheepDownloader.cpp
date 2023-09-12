13e2fdb1d620247c52621a8c8df134229f598393
trtatt <tiborhencz@Tibors-MacBook-Pro-2.local>
2023-09-12 16:19:35 +0300

    Disambiguate boost::thread and boost::this_thread

--- client_generic/ContentDownloader/SheepDownloader.cpp
+++ client_generic/ContentDownloader/SheepDownloader.cpp
@@ -780,7 +780,7 @@ void	SheepDownloader::findSheepToDownload()
 			//	Make sure we are really deeply settled asleep, avoids lots of timed out frames.
 			g_Log->Info( "Chilling for %d seconds before trying to download sheeps...", ContentDownloader::INIT_DELAY );
 			
-			thread::sleep( get_system_time() + posix_time::seconds(ContentDownloader::INIT_DELAY) );
+			boost::thread::sleep( get_system_time() + posix_time::seconds(ContentDownloader::INIT_DELAY) );
 		}
 #endif
 
@@ -793,7 +793,7 @@ void	SheepDownloader::findSheepToDownload()
 		while( 1 )
 		{
 
-			this_thread::interruption_point();
+			boost::this_thread::interruption_point();
 			bool incorrect_folder = false;
 #ifdef WIN32
 			ULARGE_INTEGER winlpFreeBytesAvailable, winlpTotalNumberOfBytes, winlpRealBytesAvailable;
@@ -820,14 +820,14 @@ void	SheepDownloader::findSheepToDownload()
 					const char *err = "Content folder is not working.  Downloading disabled.\n";
 					Shepherd::addMessageText( err, strlen(err), 180 ); //3 minutes
 
-					thread::sleep( get_system_time() + posix_time::seconds(TIMEOUT) );
+					boost::thread::sleep( get_system_time() + posix_time::seconds(TIMEOUT) );
 				}
 				else
 				{
 					const char *err = "Low disk space.  Downloading disabled.\n";
 					Shepherd::addMessageText( err, strlen(err), 180 ); //3 minutes
 
-					thread::sleep( get_system_time() + posix_time::seconds(TIMEOUT) );
+					boost::thread::sleep( get_system_time() + posix_time::seconds(TIMEOUT) );
 				
 					boost::mutex::scoped_lock lockthis( s_DownloaderMutex );
 
@@ -938,7 +938,7 @@ void	SheepDownloader::findSheepToDownload()
 								best_anim_old_url = fServerFlock[ static_cast<size_t>(best_anim_old) ]->URL();
 							}
 						}
-						this_thread::interruption_point();
+						boost::this_thread::interruption_point();
 					} while (best_anim != -1);
 
 					if (best_anim_old == -1)
@@ -976,7 +976,7 @@ void	SheepDownloader::findSheepToDownload()
 					badSheepSleepDuration = 10;
 				}
 
-				thread::sleep( get_system_time() + posix_time::seconds(failureSleepDuration) );
+				boost::thread::sleep( get_system_time() + posix_time::seconds(failureSleepDuration) );
 				
 				//failureSleepDuration = TIMEOUT;
 
