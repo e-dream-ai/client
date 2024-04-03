79b630a5a7d8e97906c8f15e7f8d97128993a2df
Guillaume Louel <37544189+glouel@users.noreply.github.com>
2024-04-03 14:43:12 +0200

    Move at launch if in download folder

--- client_generic/MacBuild/main.m
+++ client_generic/MacBuild/main.m
@@ -4,16 +4,29 @@
 
 #import <Cocoa/Cocoa.h>
 
+// MacOS Platform helpers that run at startup to handle apps launched after being
+// unzipped in Downloads folder
+void EnsureNotInDownloadFolder();
+NSString *ShellQuotedString(NSString *string);
+void Relaunch(NSString *destinationPath);
+
+/// MARK: - MacOS app client entry point
 int main(int argc, char* argv[])
 {
     // Ensure we are not launching from App folder, if so move to /Applications and relaunch
+    EnsureNotInDownloadFolder();
     
+    return NSApplicationMain(argc, (const char**)argv);
+}
+
+// Helpers
+void EnsureNotInDownloadFolder() {
     // Get current launch URL
     NSString *currentPath = [[NSBundle mainBundle] bundlePath];
     NSURL *currentURL = [NSURL fileURLWithPath:currentPath];
     
     NSString *downloadsPath = NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory, NSUserDomainMask, YES)[0];
-    NSURL *downloadsURL = [NSURL fileURLWithPath:downloadsPath];
+    NSURL *downloadsURL = [NSURL fileURLWithPath:[downloadsPath stringByAppendingPathComponent:@"e-dream.app"]];
     
     if ([currentURL isEqual:downloadsURL]) {
         // The app is in the Downloads folder.
@@ -21,20 +34,54 @@ int main(int argc, char* argv[])
         NSError *error;
         
         NSFileManager *fileManager = [NSFileManager defaultManager];
-        if ([fileManager moveItemAtURL:currentURL toURL:[NSURL fileURLWithPath:[applicationsPath stringByAppendingPathComponent:@"e-dream.app"]]
-                                   error: &error
-              ]) {
+        NSString *appPath = [applicationsPath stringByAppendingPathComponent:@"e-dream.app"];
+        NSURL *appURL = [NSURL fileURLWithPath: appPath];
+        
+        if ([fileManager fileExistsAtPath:appPath]) {
+            if (![fileManager removeItemAtPath:appPath error:&error]) {
+                NSLog(@"Could not delete old application.");
+            }
+        }
+            
+        if ([fileManager moveItemAtURL:currentURL toURL:appURL error: &error]) {
             NSLog(@"Application moved successfully.");
             
             // Reload the app from /Applications
-            NSApplication *app = [NSApplication sharedApplication];
-            [NSWorkspace.sharedWorkspace launchApplicationAtURL:[NSURL fileURLWithPath:[applicationsPath stringByAppendingPathComponent:@"e-dream.app"]] options:NSWorkspaceLaunchDefault configuration:nil error:&error];
-             
+            Relaunch(appPath);
+            
             [NSApplication.sharedApplication terminate:nil];
         } else {
             NSLog(@"Error moving application: %@", error);
         }
     }
-    
-    return NSApplicationMain(argc, (const char**)argv);
+}
+
+NSString *ShellQuotedString(NSString *string) {
+    return [NSString stringWithFormat:@"'%@'", [string stringByReplacingOccurrencesOfString:@"'" withString:@"'\\''"]];
+}
+
+void Relaunch(NSString *destinationPath) {
+    // The shell script waits until the original app process terminates.
+    // This is done so that the relaunched app opens as the front-most app.
+    int pid = [[NSProcessInfo processInfo] processIdentifier];
+
+    // Command run just before running open /final/path
+    NSString *preOpenCmd = @"";
+
+    NSString *quotedDestinationPath = ShellQuotedString(destinationPath);
+
+    // OS X >=10.5:
+    // Before we launch the new app, clear xattr:com.apple.quarantine to avoid
+    // duplicate "scary file from the internet" dialog.
+    if (floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_5) {
+        // Add the -r flag on 10.6
+        preOpenCmd = [NSString stringWithFormat:@"/usr/bin/xattr -d -r com.apple.quarantine %@", quotedDestinationPath];
+    }
+    else {
+        preOpenCmd = [NSString stringWithFormat:@"/usr/bin/xattr -d com.apple.quarantine %@", quotedDestinationPath];
+    }
+
+    NSString *script = [NSString stringWithFormat:@"(while /bin/kill -0 %d >&/dev/null; do /bin/sleep 0.1; done; %@; /usr/bin/open %@) &", pid, preOpenCmd, quotedDestinationPath];
+
+    [NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:[NSArray arrayWithObjects:@"-c", script, nil]];
 }
