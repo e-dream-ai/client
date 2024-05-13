e9594941555641baa001777da2e13b42f244cde0
Guillaume Louel <37544189+glouel@users.noreply.github.com>
2024-05-13 14:42:54 +0200

    WIP DX12
    Add DX12 Agility SDK via NuGET
    Base Display init

--- client_generic/DisplayOutput/D3D12/DisplayD3D12.cpp
+++ client_generic/DisplayOutput/D3D12/DisplayD3D12.cpp
@@ -2,9 +2,20 @@
 #include "client.h"
 #include "Log.h"
 #include "Player.h"
+#include <d3d12.h>
 
 namespace DisplayOutput
 {
+
+    using namespace DirectX;
+
+CDisplayD3D12::CDisplayD3D12() : CDisplayOutput(), m_useWarpDevice(false)
+    {
+    g_Log->Info("CDisplayD3D12()");
+}
+
+CDisplayD3D12::~CDisplayD3D12() { g_Log->Info("~CDisplayD3D12()"); }
+
 
 /*
     wndProc().
@@ -257,7 +268,8 @@ HWND CDisplayD3D12::CreateDisplayWindow(uint32_t _w, uint32_t _h, const bool _bF
     return hWnd;
 }
 
-
+// This is called in client mode, not in screensaver mode
+// We then call CreateDisplayWindow from here and return the HWND
 HWND CDisplayD3D12::Initialize(const uint32_t _width, const uint32_t _height,
                                const bool _bFullscreen)
 {
@@ -282,13 +294,205 @@ HWND CDisplayD3D12::Initialize(const uint32_t _width, const uint32_t _height,
     {
         SetForegroundWindow(m_WindowHandle);
         SetFocus(m_WindowHandle);
-    }
+    }*/
 
-    if (!InitDX9())
+   
+
+    if (!InitD3D12())
         return nullptr;
-        */
+        
     return m_WindowHandle;
 }
+
+bool CDisplayD3D12::InitD3D12()
+{
+    UINT dxgiFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    // Enable the debug layer (requires the Graphics Tools "optional feature").
+    // NOTE: Enabling the debug layer after device creation will invalidate the active device.
+    {
+        ComPtr<ID3D12Debug> debugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+        {
+            debugController->EnableDebugLayer();
+
+            // Enable additional debug layers.
+            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+        }
+    }
+#endif
+
+    ComPtr<IDXGIFactory4> factory;
+    ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory)));
+
+    if (m_useWarpDevice)
+    {
+        ComPtr<IDXGIAdapter> warpAdapter;
+        ThrowIfFailed(factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)));
+
+        ThrowIfFailed(D3D12CreateDevice(warpAdapter.Get(),
+                                        D3D_FEATURE_LEVEL_11_0,
+                                        IID_PPV_ARGS(&m_device)));
+    }
+    else
+    {
+        ComPtr<IDXGIAdapter1> hardwareAdapter;
+        GetHardwareAdapter(factory.Get(), &hardwareAdapter);
+
+        ThrowIfFailed(D3D12CreateDevice(hardwareAdapter.Get(),
+                                        D3D_FEATURE_LEVEL_11_0,
+                                        IID_PPV_ARGS(&m_device)));
+    }
+
+    // Describe and create the command queue.
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+    ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc,
+                                               IID_PPV_ARGS(&m_commandQueue)));
+
+    // Describe and create the swap chain.
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.BufferCount = FrameCount;
+    swapChainDesc.Width = m_width;
+    swapChainDesc.Height = m_height;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.SampleDesc.Count = 1;
+
+    ComPtr<IDXGISwapChain1> swapChain;
+    ThrowIfFailed(factory->CreateSwapChainForHwnd(
+        m_commandQueue
+            .Get(), // Swap chain needs the queue so that it can force a flush on it.
+        Win32Application::GetHwnd(), &swapChainDesc, nullptr, nullptr,
+        &swapChain));
+
+    // This sample does not support fullscreen transitions.
+    ThrowIfFailed(factory->MakeWindowAssociation(Win32Application::GetHwnd(),
+                                                 DXGI_MWA_NO_ALT_ENTER));
+
+    ThrowIfFailed(swapChain.As(&m_swapChain));
+    m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+    // Create descriptor heaps.
+    {
+        // Describe and create a render target view (RTV) descriptor heap.
+        D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+        rtvHeapDesc.NumDescriptors = FrameCount;
+        rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc,
+                                                     IID_PPV_ARGS(&m_rtvHeap)));
+
+        m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(
+            D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    }
+
+    // Create frame resources.
+    {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(
+            m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+        // Create a RTV for each frame.
+        for (UINT n = 0; n < FrameCount; n++)
+        {
+            ThrowIfFailed(
+                m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n])));
+            m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr,
+                                             rtvHandle);
+            rtvHandle.Offset(1, m_rtvDescriptorSize);
+        }
+    }
+
+    ThrowIfFailed(m_device->CreateCommandAllocator(
+        D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)));
+	return true;
+}
+
+// Helper function for acquiring the first available hardware adapter that supports Direct3D 12.
+// If no such adapter can be found, *ppAdapter will be set to nullptr.
+_Use_decl_annotations_ void
+CDisplayD3D12::GetHardwareAdapter(IDXGIFactory1* pFactory,
+                                  IDXGIAdapter1** ppAdapter,
+                             bool requestHighPerformanceAdapter)
+{
+    *ppAdapter = nullptr;
+
+    ComPtr<IDXGIAdapter1> adapter;
+
+    ComPtr<IDXGIFactory6> factory6;
+    if (SUCCEEDED(pFactory->QueryInterface(IID_PPV_ARGS(&factory6))))
+    {
+        for (UINT adapterIndex = 0;
+             SUCCEEDED(factory6->EnumAdapterByGpuPreference(
+                 adapterIndex,
+                 requestHighPerformanceAdapter == true
+                     ? DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE
+                     : DXGI_GPU_PREFERENCE_UNSPECIFIED,
+                 IID_PPV_ARGS(&adapter)));
+             ++adapterIndex)
+        {
+            DXGI_ADAPTER_DESC1 desc;
+            adapter->GetDesc1(&desc);
+
+            if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            {
+                // Don't select the Basic Render Driver adapter.
+                // If you want a software adapter, pass in "/warp" on the command line.
+                continue;
+            }
+
+            // Check to see whether the adapter supports Direct3D 12, but don't create the
+            // actual device yet.
+            if (SUCCEEDED(D3D12CreateDevice(adapter.Get(),
+                                            D3D_FEATURE_LEVEL_11_0,
+                                            _uuidof(ID3D12Device), nullptr)))
+            {
+                break;
+            }
+        }
+    }
+
+    if (adapter.Get() == nullptr)
+    {
+        for (UINT adapterIndex = 0;
+             SUCCEEDED(pFactory->EnumAdapters1(adapterIndex, &adapter));
+             ++adapterIndex)
+        {
+            DXGI_ADAPTER_DESC1 desc;
+            adapter->GetDesc1(&desc);
+
+            if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            {
+                // Don't select the Basic Render Driver adapter.
+                // If you want a software adapter, pass in "/warp" on the command line.
+                continue;
+            }
+
+            // Check to see whether the adapter supports Direct3D 12, but don't create the
+            // actual device yet.
+            if (SUCCEEDED(D3D12CreateDevice(adapter.Get(),
+                                            D3D_FEATURE_LEVEL_11_0,
+                                            _uuidof(ID3D12Device), nullptr)))
+            {
+                break;
+            }
+        }
+    }
+
+    *ppAdapter = adapter.Detach();
+}
+
+
+// ?? Not sure why we need this 
+bool CDisplayD3D12::Initialize()
+{
+	return true;
+}
+
 
 void CDisplayD3D12::Title(const std::string& _title)
 {
