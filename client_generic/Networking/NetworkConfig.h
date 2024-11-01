9f711fea317be1b34961137450f201e68edb50bf
Guillaume Louel <37544189+glouel@users.noreply.github.com>
2024-11-01 14:10:42 +0100

    Add extra headers to network requests

---
+++ client_generic/Networking/NetworkConfig.h
@@ -0,0 +1,36 @@
+//
+//  NetworkConfig.h
+//  e-dream
+//
+//  Created by Guillaume Louel on 01/11/2024.
+//
+
+#ifndef NetworkConfig_h
+#define NetworkConfig_h
+
+#include <string>
+#include "PlatformUtils.h"
+
+namespace Network {
+
+// Network request headers configuration
+struct NetworkHeaders {
+    static std::string getClientType() {
+        return "Edream-Client-Type: native";
+    }
+    
+    static std::string getClientVersion() {
+        return "Edream-Client-Version: " + PlatformUtils::GetAppVersion();
+    }
+    
+    // Helper method to add all standard headers to a request
+    template<typename T>
+    static void addStandardHeaders(T& downloader) {
+        downloader->AppendHeader(getClientType());
+        downloader->AppendHeader(getClientVersion());
+    }
+};
+
+} // namespace Network
+
+#endif /* NetworkConfig_h */
